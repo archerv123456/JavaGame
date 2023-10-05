89900b5b12af7101cb369bdd6bdd8546146f2e37
ANamees <87618338+archerv123456@users.noreply.github.com>
2023-10-05 09:21:39 -0500

    Unify versions

---
+++ main/com/tutorial/main/HUD.java
@@ -0,0 +1,70 @@
+package com.tutorial.main;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class HUD {
+	
+	public static int bounds = 0;
+	public static float HEALTH = 100;
+	
+	private static float greenValue = 255f;
+	static int score = 0;
+	private static float level = 0;
+	
+	public static float reviveTimer = 0;
+	public static boolean canRevive = false;
+	
+	public static void tick() {
+		
+		
+		HEALTH = Game.clamp(HEALTH, 0, 100+(bounds/2));
+		HUD.greenValue = Game.clamp(HUD.greenValue, 0, 255);
+		
+		HUD.greenValue = HEALTH*2;
+		score++;
+		
+		if (HUD.HEALTH <= 0) {
+			if (reviveTimer >= 100) {
+				canRevive = true;
+			} else if (reviveTimer < 100) {
+				reviveTimer++;
+			}
+		}
+	}
+	
+	public void render(Graphics g) {
+		g.setColor(Color.gray);
+		g.fillRect(15, 15, 200 + bounds, 32);
+		g.setColor(new Color(75, (int)greenValue, 0));
+		g.fillRect(15, 15, (int)HEALTH * 2, 32);
+		g.setColor(Color.white);
+		g.drawRect(15, 15, 200 + bounds, 32);
+		
+		g.drawString("Score: " + score, 15, 64);
+		g.drawString("Level: " + level, 15, 80);
+		g.drawString("Player1", 15, 13);
+		
+		if(HEALTH <= 0) {
+			g.setColor(Color.WHITE);
+			g.drawString("Player1 Died.", 15, 100);
+		}
+	}
+	
+	public static void setScore(int score) {
+		HUD.score = score;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+	
+	public float getLevel() {
+		return level;
+	}
+	
+	public static void setLevel(float level) {
+		HUD.level = level;
+	}
+	
+}
---
+++ main/com/tutorial/main/ID.java
@@ -0,0 +1,22 @@
+package com.tutorial.main;
+
+public enum ID {
+	
+	Player(),
+	Player2(),
+	Player3(),
+	Player4(),
+	BasicEnemy(),
+	SlowEnemy(),
+	HardEnemy(),
+	LunaiticEnemy(),
+	FastEnemy(),
+	Friend(),
+	ShooterEnemy(),
+	SmartEnemy(),
+	EnemyBoss(),
+	MenuParticle(),
+	EnemyBoss2(),
+	Trail();
+	
+}
