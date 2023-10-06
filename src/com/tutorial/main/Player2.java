a009338a9526f4d39c990cdb25a1f80314d74903
ANamees <87618338+archerv123456@users.noreply.github.com>
2023-10-06 09:33:06 -0500

    V0.0.13

---
+++ src/com/tutorial/main/Player2.java
@@ -0,0 +1,148 @@
+package com.tutorial.main;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Player2 extends GameObject{
+	
+	Handler handler;
+	private long lastCollisionTime = 0;
+	private long collisionCooldown = 500; // Cooldown in milliseconds (1 second in this example)
+	private boolean isBlinking = false;
+	private int blinkInterval = 200; // Blink interval in milliseconds
+	private boolean shouldRenderPlayer = true;
+
+	public Player2(int x, int y, ID id, Handler handler) {
+		super(x, y, id);
+		this.handler = handler;
+	}
+	
+	public Rectangle getBounds() {
+		return new Rectangle((int)x, (int)y, 16, 16);
+	}
+
+	
+	public void tick() {
+		x += velX;
+		y += velY; 
+		
+		x = Game.clamp(x, 0, Game.WIDTH - 48);
+		y = Game.clamp(y, 0, Game.HEIGHT - 71);
+
+		
+		collision();
+		
+		if(HUD2.HEALTH <= 0) {
+			handler.removeObject(this);
+		}
+	}
+	
+	
+
+	public void render(Graphics g) {
+	    if (!isBlinking) {
+	        g.setColor(Color.blue);
+	        g.fillRect((int)x, (int)y, 32, 32);
+	    }
+	}
+	
+	private void collision() {
+long currentTime = System.currentTimeMillis();
+		
+		
+	    
+	    for(int i = 0; i < handler.object.size(); i++) {
+	
+	
+			
+			GameObject tempObject = handler.object.get(i);
+		    
+		    
+			
+			if(tempObject.getId() == ID.BasicEnemy) {
+				if(getBounds().intersects(tempObject.getBounds())) {
+					HUD2.HEALTH -= 35;
+					lastCollisionTime = currentTime;
+				}
+			}
+			
+			else if(tempObject.getId() == ID.SlowEnemy) {
+				if(getBounds().intersects(tempObject.getBounds())) {
+					HUD2.HEALTH -= 51;
+					lastCollisionTime = currentTime;
+			}	
+		}
+				
+			else if(tempObject.getId() == ID.Friend) {
+					if(getBounds().intersects(tempObject.getBounds())) {
+						HUD2.HEALTH -= 50;
+						lastCollisionTime = currentTime;
+				}	
+			}
+			else if(tempObject.getId() == ID.FastEnemy) {
+					if(getBounds().intersects(tempObject.getBounds())) {
+						HUD2.HEALTH -= 35;
+						lastCollisionTime = currentTime;
+				} 
+			}
+			else if(tempObject.getId() == ID.SmartEnemy) {
+					if(getBounds().intersects(tempObject.getBounds())) {
+						HUD2.HEALTH -= 35;
+						lastCollisionTime = currentTime;
+				}
+			}
+				
+			else if(tempObject.getId() == ID.EnemyBoss) {
+					if(getBounds().intersects(tempObject.getBounds())) {
+						HUD2.HEALTH -= 100;
+						lastCollisionTime = currentTime;
+				}
+			}
+				
+			else if(tempObject.getId() == ID.HardEnemy) {
+					if(getBounds().intersects(tempObject.getBounds())) {
+						HUD2.HEALTH -= 35;
+						lastCollisionTime = currentTime;
+				}
+			}
+			else if(tempObject.getId() == ID.LunaiticEnemy) {
+				if(getBounds().intersects(tempObject.getBounds())) {
+					HUD2.HEALTH -= 35;
+					lastCollisionTime = currentTime;
+				}
+			}
+			else if (currentTime - lastCollisionTime < collisionCooldown) {
+		        if (!isBlinking) {
+		            isBlinking = true;
+		            startBlinking();
+		        }
+		        return; 
+		    }
+
+					
+				if (isBlinking) {
+			        isBlinking = false;
+			        stopBlinking();
+			    }
+		}
+	}
+	
+	private void startBlinking() {
+	    Timer timer = new Timer();
+	    timer.scheduleAtFixedRate(new TimerTask() {
+	        @Override
+	        public void run() {
+	            shouldRenderPlayer = !shouldRenderPlayer; // Toggle rendering on and off
+	        }
+	    }, 0, blinkInterval);
+	}
+
+	private void stopBlinking() {
+	    // Reset the shouldRenderPlayer flag and cancel the blinking timer
+	    shouldRenderPlayer = true;
+	}
+
+}
